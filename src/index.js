45139531b0ac1b608526b2d3e5dbd616b2bceb7d
Scott Wade <sc0ttwad3@gmail.com>
2018-01-31 11:47:47 -0500

    Alter npm scripts to build and run with start script

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { _, it } from 'param.macro';
 import chalk from "chalk";
 
-const logBlue = console.log(chalk.blue(..._));
-const logYellow = console.log(chalk.yellow(..._));
-const log = console.log(chalk.grey(..._));
+// simple logger
+const log = (..._arg) => {
+  return console.log(chalk.grey(..._arg))
+}
 
+// begin
 log("index file start...");
